7e7f80a45590c8e0149a2f218f30648dc46031e5
Seungrok Han <muster22@naver.com>
2013-11-11 23:32:10 +0900

    FirstCommit
    FirstCommit with SourceCode

---
+++ pi_servo.py
@@ -0,0 +1,35 @@
+import time
+import Servo
+
+def main():
+    servo = Servo.SERVO(9600, 2.0)
+
+    # Initial Position
+    UDpos=90
+    LRpos=90
+
+    # loop part
+    while(True):
+
+        # Increase Position
+        UDpos += 1
+        LRpos += 1
+
+        # Limit Position Set
+        if ( UDpos > 180 ):
+            UDpos = 10
+        if ( LRpos > 180 ):
+            LRpos = 10
+
+        # Some Error Check
+        rtc = True
+        rtc = servo.setUDPosition(UDpos)
+        if rtc is False:
+            print "false:%d" % UDpos
+        rtc = True
+        rtc = servo.setLRPosition(LRpos)
+        if rtc is False:
+            print "false:%d" % LRpos
+
+if __name__ == "__main__":
+    main()
